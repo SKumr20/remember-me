2de93860ab6ebef18f593d7fa9ab3315be97871d
Satyam Kumar <113851151+SKumr20@users.noreply.github.com>
2025-02-27 00:13:09 +0530

    Add random movie fetching

--- src/services/fetchPopularMovies.jsx
+++ src/services/fetchPopularMovies.jsx
@@ -3,7 +3,16 @@ const TMDB_BASE_URL = 'https://api.themoviedb.org/3';
 
 const fetchPopularMovies = async (count = 8) => {
     try {
-        const response = await fetch(`${TMDB_BASE_URL}/movie/popular?api_key=${API_KEY}`);
+
+        // fetch random page of movies
+        const pageResponse = await fetch(`${TMDB_BASE_URL}/movie/popular?api_key=${API_KEY}`);
+        const pageData = await pageResponse.json();
+        const totalPages = Math.min(pageData.total_pages, 500);
+
+        // random page
+        const randomPage = Math.floor(Math.random() * totalPages) + 1;
+
+        const response = await fetch(`${TMDB_BASE_URL}/movie/popular?api_key=${API_KEY}&page=${randomPage}`);
         const data = await response.json();
         return data.results.slice(0, count);
     } catch (error) {
